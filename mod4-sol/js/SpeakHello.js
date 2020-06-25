fb72e8a71a280f7f30b7cdd35028d1f6360f772f
cettinahalsey <50074779+cettinahalsey@users.noreply.github.com>
2020-06-25 08:35:01 -0500

    Update SpeakHello.js

--- mod4-sol/js/SpeakHello.js
+++ mod4-sol/js/SpeakHello.js
@@ -15,7 +15,8 @@
     // helloSpeaker object instead of being a standalone function.
     // See Lecture 52, part 2
     helloSpeaker.speak = function(name){
-        document.writeln(speakWord + " " + name + "<br>");
+        //document.writeln(speakWord + " " + name + "<br>");
+        console.log(speakWord + " " + name + "<br>");
     }
 
     // STEP 5: Expose the 'helloSpeaker' object to the global scope. Name it
