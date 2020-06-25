1f8dddd8152bfd194f006d9a9c713ec0afe21f18
cettinahalsey <50074779+cettinahalsey@users.noreply.github.com>
2020-06-25 09:01:41 -0500

    Update SpeakGoodBye.js

--- mod4-sol/js/SpeakGoodBye.js
+++ mod4-sol/js/SpeakGoodBye.js
@@ -18,7 +18,10 @@ var speakWord = "Good Bye";
 // byeSpeaker object instead of being a standalone function.
 // See Lecture 52, part 2
  byeSpeaker.speak = function(name) {
-  document.writeln(speakWord + " " + name + "<br>");
+  
+  //document.writeln(speakWord + " " + name + "<br>");
+  //console.log(speakWord + " " + name + "<br>");
+  return (speakWord + " " + name + "<br>");
 }
 
 // STEP 9: Expose the 'byeSpeaker' object to the global scope. Name it
