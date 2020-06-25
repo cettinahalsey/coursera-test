229a6567f91e7263a57250a920e865ca18aad10e
cettinahalsey <50074779+cettinahalsey@users.noreply.github.com>
2020-06-25 09:03:34 -0500

    Update script.js

--- mod4-sol/js/script.js
+++ mod4-sol/js/script.js
@@ -39,6 +39,7 @@ var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula"
 // using the 'speak' method or either helloSpeaker's or byeSpeaker's
 // 'speak' method.
 // See Lecture 50, part 1
+var text = "";
 for (var i =0; i<names.length; i++) {
 
   // STEP 11:
@@ -56,9 +57,16 @@ for (var i =0; i<names.length; i++) {
   // in the loop. Otherwise, call helloSpeaker's 'speak' method with the current
   // name in the loop.
   if (names[i].charAt(0).toLowerCase()==='j') {
-     byeSpeaker.speak(names[i])
+     text+=byeSpeaker.speak(names[i])
   } else {
-     helloSpeaker.speak(names[i])
+     text+=helloSpeaker.speak(names[i])
   }
+  
+  window.text = text;
 }
 })(window);
+
+
+var result = document.createElement("div");
+result.innerHTML = text;
+document.body.appendChild(result);
